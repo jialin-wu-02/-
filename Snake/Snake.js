7934d2fa5944b5198276eff5b65b10d39b1517c3
jialin wu <jialin.wu.02@gmail.com>
2019-04-29 13:11:17 -0700

    finish basic functions of snake

--- Snake/Snake.js
+++ Snake/Snake.js
@@ -1,38 +1,18 @@
-var canvas = document.querySelector("canvas");
-
-var c = canvas.getContext("2d");
-
-const unit = 40;
+const unit = 20;
 const height = 600;
 const width = 600;
 
-let leftdirection;
-let rightdirection;
-let updirection;
-let downdirection;
+let start = false;
+let foodEaten = true;
 
-window.addEventListener("keydown", (event) => {
-	console.log(event.keyCode);
-	// 37, 38, 39, 40
-	// left, up, right, down
-	if (event.keyCode == 37 && !rightdirection) {
-		leftdirection = true;
-		updirection = false;
-		downdirection = false;
-	} else if (event.keyCode == 39 && !leftdirection) {
-		rightdirection = true;
-		updirection = false;
-		downdirection = false;
-	} else if (event.keyCode == 38 && !downdirection) {
-		rightdirection = false;
-		leftdirection = false;
-		updirection = true;
-	} else if (event.keyCode == 40 && !updirection) {
-		rightdirection = false;
-		leftdirection = false;
-		downdirection = true;
-	}
-})
+let leftdirection = false;
+let rightdirection = false;
+let updirection = false;
+let downdirection = false;
+
+let canvas = document.querySelector("canvas");
+
+let c = canvas.getContext("2d");
 
 const drawBoard = () => {
 	c.strokeStyle = "grey";
@@ -47,61 +27,139 @@ const drawBoard = () => {
     c.stroke();
 }
 
-drawBoard();
 
-class Square {
-
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-	}
-
-	draw() {
-		// console.log(unit, this.x);
-		c.fillStyle = "blue";
-		c.fillRect(this.x * unit, this.y * unit, unit, unit);
-	}
-
-	moveRight() {
-		this.x = this.x + 1;
-	}
-}
-
-
-const clearBoard = () => {
+const clearBoard = (x, y) => {
 	c.clearRect(0, 0, height, width);
 	drawBoard();
 }
 
-var x = 0;
-var y = 2;
-let a = new Square(x, y);
-let b = new Square(x + 1, y);
+const drawSquare = (x, y, color) => {
+	c.fillStyle = color;
+	c.fillRect(x * unit, y * unit, unit, unit);
+}
+
+const drawSnake = (snake) => {
+	drawSquare(snake[0].x, snake[0].y, "blue");
+	for (var i = 1; i < snake.length; i++) {
+		drawSquare(snake[i].x, snake[i].y, "blue");
+	}
+}
+
+const getNewFoodPosition = () => {
+	let randomX = Math.floor(Math.random() * (width / unit - 1));
+	let randomY = Math.floor(Math.random() * (height / unit - 1));
+	if (snakeOverlap(snake, randomX, randomY)) {
+		return getNewFoodPosition();
+	} else {
+		foodEaten = false;
+		return [randomX, randomY];
+	}
+}
+
+const snakeOverlap = (snake, randomX, randomY) => {
+	for (let i = 0; i < snake.length; i++) {
+		if (snake[i].x === randomX && snake[i].y === randomY) {
+			return true;
+		}
+	}
+	return false;
+}
+
+const move = () => {
+	let dx = 0;
+	let dy = 0;
+	if (rightdirection) {
+		dx = 1;
+	} else if (leftdirection) {
+		dx = -1;
+	} else if (updirection) {
+		dy = -1;
+	} else if (downdirection) {
+		dy = 1;
+	}
+	for (let i = snake.length - 1; i >= 1; i--) {
+		snake[i].x = snake[i-1].x;
+		snake[i].y = snake[i-1].y;
+	}
+	snake[0].x += dx;
+	snake[0].y += dy;
+}
+
+let snake = [];
+let x = Math.floor(Math.random() * (width / unit - 3));
+let y = Math.floor(Math.random() * (height / unit - 3));
+
+snake[0] = {x : x + 3, y: y}; // always the head of the snake
+snake[1] = {x : x + 2, y: y};
+snake[2] = {x : x + 1, y: y};
+snake[3] = {x : x, y: y};
+
+let food = getNewFoodPosition();
+
+window.addEventListener("keydown", (event) => {
+	console.log(event.keyCode);
+	// 37, 38, 39, 40
+	// left, up, right, down
+
+	// when user press left in the start, snake needs to inverse its head.
+	if (event.keyCode == 37 && start == false) {
+		start = true;
+		snake.reverse();
+	}
+	if (event.keyCode == 37 && !rightdirection) {
+		start = true;
+		leftdirection = true;
+		updirection = false;
+		downdirection = false;
+	} else if (event.keyCode == 39 && !leftdirection) {
+		start = true;
+		rightdirection = true;
+		updirection = false;
+		downdirection = false;
+	} else if (event.keyCode == 38 && !downdirection) {
+		start = true;
+		rightdirection = false;
+		leftdirection = false;
+		updirection = true;
+	} else if (event.keyCode == 40 && !updirection) {
+		start = true;
+		rightdirection = false;
+		leftdirection = false;
+		downdirection = true;
+	}
+})
+
 
 const animate = () => {
 	clearBoard();
-	// a.draw();
-	b.draw();
-	// b.draw();
-	if (rightdirection) {
-		a.moveRight();
-		a.draw();
-	} 
-	// else if (leftdirection) {
-		// x -= 1;
-	// } else if (updirection) {
-	// 	y -= 1;
-	// } else if (downdirection) {
-	// 	y += 1;
-	// }
+	if (foodEaten) {
+		food = getNewFoodPosition();
+	}
+	drawSquare(food[0], food[1], "red");
+	drawSnake(snake);
+	if (snake[0].x == food[0] && snake[0].y == food[1]) {
+		foodEaten = true;
+		snake[snake.length] = {x : snake[snake.length - 1].x, y: snake[snake.length - 1].y};
+		snake[snake.length] = {x : snake[snake.length - 1].x, y: snake[snake.length - 1].y};
+	}
+
+	// eat itself
+	if (snakeOverlap(snake.slice(1, snake.length), snake[0].x, snake[0].y)) {
+		clearInterval(timer);
+	}
+
+	// edges detection
+	if (snake[0].x < 0 || snake[0].y < 0 
+		|| snake[0].x > width / unit - 1 
+		|| snake[0].y > height / unit - 1) {
+		clearInterval(timer);
+	}
+	if (start) {
+		move();
+	}
 }
 
-setInterval(animate, 150);
-
-
-
-
-
+var timer = setInterval(animate, 110);
 
 
 
